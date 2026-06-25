37cf1208804fc6963ab7a0e390525b9009a14562
notluauorg-a11y <notluauorg@gmail.com>
2026-06-26 05:19:54 +0700

    Delete dashboard.js

--- dashboard.js
+++
@@ -1,7 +0,0 @@
-$done({
- status: 200,
- headers: {
-  "Content-Type": "application/json; charset=utf-8"
- },
- body: `{"success":true,"message":"Success!","email":"omg is this federal","discord_id":"67","expires_at":2080394667,"registered_at":67,"plan":"e","enabled":1,"projects":[]}`
-});
