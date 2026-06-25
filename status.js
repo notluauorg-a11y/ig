bf8a937b1adb0a9f8052194206abb2392a6fdbd2
notluauorg-a11y <notluauorg@gmail.com>
2026-06-26 05:20:20 +0700

    Delete status.js

--- status.js
+++
@@ -1,7 +0,0 @@
-$done({
- status: 200,
- headers: {
-  "Content-Type": "application/json; charset=utf-8"
- },
- body: `{"version":"v3","active":true,"message":"1337 boiiii 😂😂","warning":false,"warning_message":"nothing to see here."}`
-});
