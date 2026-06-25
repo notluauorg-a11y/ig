d9cc499f2fe1aa786ad2726c03c5e1439d5ce459
notluauorg-a11y <notluauorg@gmail.com>
2026-06-26 05:20:04 +0700

    Delete wAG.js

--- wAG.js
+++
@@ -1,7 +0,0 @@
-$done({
- status: 200,
- headers: {
-  "Content-Type": "application/json; charset=utf-8"
- },
- body: `{"success":true,"message":"Success!","execution_data":{"frequency":86400,"executions":[0]},"stats":{"obfuscations":0,"scripts":0,"users":0,"attacks_blocked":0,"default":{"scripts":100,"users":500000,"obfuscations":9999},"reset_at":2080394667}}`
-});
