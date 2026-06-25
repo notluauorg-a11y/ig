dc054206ae1e3ab1238e882cad9bb62173d3e376
igvro838 <notluauorg@gmail.com>
2026-06-26 05:37:16 +0700

    Update wAG.js

--- wAG.js
+++ wAG.js
@@ -3,5 +3,5 @@ $done({
  headers: {
   "Content-Type": "application/json; charset=utf-8"
  },
- body: `{"success":true,"message":"Success!","execution_data":{"frequency":86400,"executions":[74719472]},"stats":{"obfuscations":1,"scripts":1,"users":0,"attacks_blocked":472941,"default":{"scripts":100,"users":500000,"obfuscations":9999},"reset_at":0}}`
+ body: `{"success":true,"message":"Success!","execution_data":{"frequency":86400,"executions":[7471947]},"stats":{"obfuscations":1,"scripts":1,"users":500000,"attacks_blocked":472941,"default":{"scripts":100,"users":500000,"obfuscations":9999},"reset_at":2505600}}`
 });
