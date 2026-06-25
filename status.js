980e1c98e06be2b637e4ee28b3df5b6cf14f301b
notluauorg-a11y <notluauorg@gmail.com>
2026-06-26 04:45:24 +0700

    Add files via upload

---
+++ status.js
@@ -0,0 +1,7 @@
+$done({
+ status: 200,
+ headers: {
+  "Content-Type": "application/json; charset=utf-8"
+ },
+ body: `{"version":"v3","active":true,"message":"1337 boiiii 😂😂","warning":false,"warning_message":"nothing to see here."}`
+});
