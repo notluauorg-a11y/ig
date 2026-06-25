61af23d5c97ddb8311c231c204f069addeeec41d
igvro838 <notluauorg@gmail.com>
2026-06-26 05:40:04 +0700

    Update nU.js

--- nU.js
+++ nU.js
@@ -3,5 +3,5 @@ $done({
  headers: {
   "Content-Type": "application/json; charset=utf-8"
  },
- body: `{"success":true,"message":"Success!","execution_data":{"frequency":86400,"executions":[0]},"stats":{"obfuscations":1,"scripts":1,"default":{"scripts":100,"users":500000,"obfuscations":9999},"reset_at":2505600}}`
+ body: `{"success":true,"message":"Success!","execution_data":{"frequency":86400,"executions":[7471947]},"stats":{"obfuscations":1,"scripts":1,"default":{"scripts":100,"users":500000,"obfuscations":9999},"reset_at":2505600}}`
 });
