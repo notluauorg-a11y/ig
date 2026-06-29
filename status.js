649c7179a2f766d34b6be7606540b399d45e65c1
igvro838 <notluauorg@gmail.com>
2026-06-29 12:25:54 +0700

    Update status.js

--- status.js
+++ status.js
@@ -3,5 +3,5 @@ $done({
  headers: {
   "Content-Type": "application/json; charset=utf-8"
  },
- body: `{"version":"v3","active":true,"message":"1337 boiiii 😂😂","warning":false,"warning_message":"nothing to see here."}`
+ body: `{"version":"v3","active":true,"message":"API is up and working!","warning":false,"warning_message":"nothing to see here."}`
 });
